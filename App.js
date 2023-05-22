62878a5911af6f889f2af6752d22b54c71bb905b
ShawnBlair <shawnmwa98@gmail.com>
2023-05-22 21:34:22 +0300

    The 'boxes challenge' project

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from "react"
+import Box from "./Box"
+import boxes from "./boxes"
+
+export default function App(){
+    
+    const [bx, setBx] = React.useState(boxes)
+    
+    function toggle(id){
+        setBx(bx=> {
+            return bx.map(box=> box.id === id ?{...box,on: !box.on}: box)})
+    }   
+     
+    const bxes =  bx.map(box =>    
+        {return(
+            <Box 
+                key={box.id} 
+                on={box.on} 
+                handleClick={() => toggle(box.id)} 
+            />)}
+            
+    )    
+    return(
+        <main>{bxes}</main>        
+    )    
+}
+
+
